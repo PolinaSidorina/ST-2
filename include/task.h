33feb063251ac4417b7e08c93559c5d1a57c9bae
Polina Sidorina <polishas206@gmail.com>
2025-03-09 23:23:00 +0300

    2.8

--- include/task.h
+++ include/task.h
@@ -1,10 +1,9 @@
-// Copyright 2025 UNN-CS_
-#ifndef INCLUDE_TASK_H_  
+// Copyright 2025 UNN-CS
+
+#ifndef INCLUDE_TASKS_H_
 #define INCLUDE_TASKS_H_
-#include "circle.h"
 
 double Earth_and_Roop();
-
 double Pool();
 
-#endif  // INCLUDE_TASK_H_
+#endif // INCLUDE_TASKS_H_
